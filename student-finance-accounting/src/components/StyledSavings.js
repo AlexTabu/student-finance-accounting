4baff53accc2c9a8801196ec92adde515656bae5
Alex Tabu <a.taburov@intetics.com>
2023-11-28 12:21:24 +0200

    Add AnimatedContainer to StyledSavings

--- student-finance-accounting/src/components/StyledSavings.js
+++ student-finance-accounting/src/components/StyledSavings.js
@@ -1,22 +1,23 @@
 import React from "react";
+import AnimatedContainer from "./AnimatedContainer";
 
-const StyledSavings = ({saving}) => {
+const StyledSavings = ({ saving }) => {
     return (
-        <div
-            className="flex flex-row p-4 bg-black rounded-full"
-            data-aos="fade-up"
-            data-aos-offset="0"
-            data-aos-delay={saving.animationDelay}
-            data-aos-duration="800"
-            data-aos-once="true"
-        >
-            <img
-                className="h-10 w-10"
-                src={saving.src}
-                alt={saving.alt}
-            />
-            <h1 className="ms-3 text-4xl font-bold">{saving.savings}</h1>
-        </div>
+        <AnimatedContainer
+            element={
+                <div
+                    className="flex flex-row p-4 bg-black rounded-full"
+                >
+                    <img
+                        className="h-10 w-10"
+                        src={saving.src}
+                        alt={saving.alt}
+                    />
+                    <h1 className="ms-3 text-4xl font-bold">{saving.savings}</h1>
+                </div>
+            }
+            animationDelay={saving.animationDelay}
+        />
     );
 }
 
