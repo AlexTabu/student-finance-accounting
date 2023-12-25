7674833edd1136385162c4d00988ecb696603517
Alex Tabu <a.taburov@intetics.com>
2023-12-25 15:08:11 +0200

    Modify StyledButton, add it on AuthPage

--- student-finance-accounting/src/components/StyledButton.js
+++ student-finance-accounting/src/components/StyledButton.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const StyledButton = ({text, hoverStyle, clickHandler}) => {
+const StyledButton = ({text, type, width, marginTop, hoverStyle, clickHandler}) => {
     return (
         <button
-            className={`focus:outline-none focus:ring-4 focus:ring-purple-300
-            text-white bg-black ${hoverStyle} transition-all duration-300
-            font-medium rounded-md text-sm px-5 py-2.5`}
+            className={`${width} ${marginTop} focus:outline-none focus:ring-4 focus:ring-purple-300
+                text-white bg-black ${hoverStyle} transition-all duration-300
+                font-medium rounded-md text-sm px-5 py-2.5`}
+            type={type}
             onClick={clickHandler}
         >
             {text}
--- student-finance-accounting/src/pages/AuthPage.js
+++ student-finance-accounting/src/pages/AuthPage.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { observer } from "mobx-react-lite";
 import { useNavigate } from "react-router-dom";
 import appStore from "../stores/AppStore";
+import StyledButton from "../components/StyledButton";
+import { purpleHoverStyle } from "../constants";
 
 const AuthPage = observer(() => {
     const [login, setLogin] = useState('');
@@ -72,15 +74,16 @@ const AuthPage = observer(() => {
                                     />
                                 </div>
                             </div>
-                            <div>
-                                <button
+                            <div
+                                className="flex justify-center"
+                            >
+                                <StyledButton
+                                    width='w-full'
+                                    marginTop='mt-5'
+                                    text='Sign in'
                                     type="submit"
-                                    className="mt-6 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm
-                                        font-semibold leading-6 text-white shadow-sm hover:bg-indigo-700 focus-visible:outline
-                                        focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                >
-                                    Sign in
-                                </button>
+                                    hoverStyle={purpleHoverStyle}
+                                />
                             </div>
                         </form>
                     </div>
