5e35b5449e3ed5b7a1bf84ce06ce02a5cad87b22
Alex Tabu <a.taburov@intetics.com>
2024-01-04 20:53:32 +0200

    Edit table extension behavior

--- student-finance-accounting/src/components/ReportTable.js
+++ student-finance-accounting/src/components/ReportTable.js
@@ -59,72 +59,74 @@ const ReportTable = observer(() => {
                         ].map((item, index) => <StyledSavings key={index} saving={item} />)}
                     </div>
                 </div>
-                <table className="w-full text-sm text-left rtl:text-right text-black">
-                    <thead className="uppercase bg-teal-300 font-bold">
-                        <tr>
-                            <th scope="col" className="px-6 py-3" />
-                            <th scope="col" className="px-6 py-3">
-                                Month
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Income
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Expenses
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Type
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Savings in UAH
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Savings in USD
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Savings in EUR
-                            </th>
-                            <th scope="col" className="px-6 py-3" />
-                            <th scope="col" className="px-6 py-3" />
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {userStore.reports.slice().sort((a, b) => a.monthData.id - b.monthData.id).map((report, index) =>
-                            <tr key={index} className="bg-teal-300 font-bold border-b-2 border-black">
-                                <td className="px-6 py-4">
-                                    {report.monthData.id}
-                                </td>
-                                <td className="px-6 py-4">
-                                    {report.monthData.name}
-                                </td>
-                                <td className="px-6 py-4">
-                                    {report.incomeUah}
-                                </td>
-                                <td className="px-6 py-4">
-                                    {report.expensesUah}
-                                </td>
-                                <td className="px-6 py-4">
-                                    {report.expenseData.name}
-                                </td>
-                                <td className="px-6 py-4">
-                                    {report.savingsUah}
-                                </td>
-                                <td className="px-6 py-4">
-                                    {report.savingsUsd}
-                                </td>
-                                <td className="px-6 py-4">
-                                    {report.savingsEur}
-                                </td>
-                                <td className="px-6 py-4">
-                                    <StyledButton text='Edit' hoverStyle={purpleHoverStyle} clickHandler={() => setMonthReportToModify(report)} />
-                                </td>
-                                <td className="px-6 py-4">
-                                    <StyledButton text='Delete' hoverStyle={purpleHoverStyle} clickHandler={() => userStore.deleteReport(report)} />
-                                </td>
+                <div className="overflow-x-auto">
+                    <table className="w-full text-sm text-left rtl:text-right text-black">
+                        <thead className="uppercase bg-teal-300 font-bold">
+                            <tr>
+                                <th scope="col" className="px-6 py-3" />
+                                <th scope="col" className="px-6 py-3">
+                                    Month
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Income
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Expenses
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Type
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Savings in UAH
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Savings in USD
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Savings in EUR
+                                </th>
+                                <th scope="col" className="px-6 py-3" />
+                                <th scope="col" className="px-6 py-3" />
                             </tr>
-                        )}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {userStore.reports.slice().sort((a, b) => a.monthData.id - b.monthData.id).map((report, index) =>
+                                <tr key={index} className="bg-teal-300 font-bold border-b-2 border-black">
+                                    <td className="px-6 py-4">
+                                        {report.monthData.id}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {report.monthData.name}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {report.incomeUah}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {report.expensesUah}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {report.expenseData.name}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {report.savingsUah}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {report.savingsUsd}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {report.savingsEur}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        <StyledButton text='Edit' hoverStyle={purpleHoverStyle} clickHandler={() => setMonthReportToModify(report)} />
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        <StyledButton text='Delete' hoverStyle={purpleHoverStyle} clickHandler={() => userStore.deleteReport(report)} />
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
                 {monthReportToModify &&
                     <ModifyFinancialReportModal report={monthReportToModify} handleClose={() => setMonthReportToModify(null)} />
                 }
